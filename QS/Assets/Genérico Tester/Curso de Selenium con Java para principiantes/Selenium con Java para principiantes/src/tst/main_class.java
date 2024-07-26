d8af47d8ae47461883a9c7d02584ebaccbddfc26
Ovejero-Pujol Unai <unai.ovejero.ext@inetum.com>
2024-07-26 08:34:36 +0200

    00F

---
+++ QS/Assets/Genérico Tester/Curso de Selenium con Java para principiantes/Selenium con Java para principiantes/src/tst/main_class.java
@@ -0,0 +1,31 @@
+//
+package tst;
+import org.openqa.selenium.By;
+//
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//
+public class main_class {
+        public static void main(String[] args) {
+            //
+            System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            //
+            try {
+                driver.get("https://www.lttstore.com");
+                //
+                Thread.sleep(5000);
+                //
+                driver.findElement(By.xpath("//*[@id=\"cart-icon-bubble\"]")).click();
+                //
+                String title = driver.getTitle();
+                //
+                System.out.println(title);
+            }catch(Exception e) {
+                e.printStackTrace();
+            }finally {
+                driver.quit();
+            }
+        }
+}
+//
